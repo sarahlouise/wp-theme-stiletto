560622ce286e25287ce4aef8bc5a8d9b33fcf18a
Sarah Kruberg <sarah.kruberg@gmail.com>
2015-07-27 22:40:33 -0700

    added tumblr plugin and imported posts from MOOD tumblr account

--- wp-content/themes/twentyfifteen/header.php
+++ wp-content/themes/twentyfifteen/header.php
@@ -32,14 +32,20 @@
 					if ( is_front_page() && is_home() ) : ?>
 						<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 					<?php else : ?>
+                                                
 						<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-					<?php endif;
+					
+                                        <?php endif;
 
 					$description = get_bloginfo( 'description', 'display' );
-					if ( $description || is_customize_preview() ) : ?>
-						<p class="site-description"><?php echo $description; ?></p>
-					<?php endif;
+					
+                                        if ( $description || is_customize_preview() ) : ?>
+						
+                                                <p class="site-description"><?php echo $description; ?></p>
+					
+                                       <?php endif;
 				?>
+                                                
 				<button class="secondary-toggle"><?php _e( 'Menu and widgets', 'twentyfifteen' ); ?></button>
 			</div><!-- .site-branding -->
 		</header><!-- .site-header -->
