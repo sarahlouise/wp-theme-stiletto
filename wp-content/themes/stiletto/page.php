c2dd2d39c24314542b1030af6558d80d2f1a97a1
Sarah Kruberg <sarah.kruberg@gmail.com>
2015-07-27 23:47:12 -0700

    removed get_footer() from content area and added closing divs from footer.php. Created a full page content section for static pages.

--- wp-content/themes/stiletto/page.php
+++ wp-content/themes/stiletto/page.php
@@ -13,26 +13,32 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<div id="primary" class="content-area">
+			<main id="main" class="site-main" role="main">
 
-		<?php
-		// Start the loop.
-		while ( have_posts() ) : the_post();
+				<?php
+				// Start the loop.
+				while ( have_posts() ) : the_post();
 
-			// Include the page content template.
-			get_template_part( 'content', 'page' );
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
 
-			// *inactive* If comments are open or we have at least one comment, load up the comment template.
-			// if ( comments_open() || get_comments_number() ) :
-			// 	comments_template();
-			// endif;
+					// *inactive* If comments are open or we have at least one comment, load up the comment template.
+					// if ( comments_open() || get_comments_number() ) :
+					// 	comments_template();
+					// endif;
 
-		// End the loop.
-		endwhile;
-		?>
+				// End the loop.
+				endwhile;
+				?>
 
-		</main><!-- .site-main -->
-	</div><!-- .content-area -->
+				<!-- ADDED below from original footer.php to close remaining open divs -->
+			</main><!-- .site-main -->
+		</div><!-- .content-area -->
+	</div><!-- .site-content -->
+</div><!-- .site -->
 
-<?php get_footer(); ?>
+	<?php wp_footer(); ?>
+
+</body>
+</html>
